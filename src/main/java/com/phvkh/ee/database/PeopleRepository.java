607a6f243eea45aa12af500f4850d59340b68373
Philip <philipvakh@gmail.com>
2019-12-03 13:59:56 +0300

    removed trash

--- src/main/java/com/phvkh/ee/database/PeopleRepository.java
+++ src/main/java/com/phvkh/ee/database/PeopleRepository.java
@@ -1,28 +1,14 @@
 package com.phvkh.ee.database;
-import com.fasterxml.classmate.AnnotationConfiguration;
 import com.phvkh.ee.dto.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import java.sql.*;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class PeopleRepository {
     private static PeopleRepository instance;
     private SessionFactory sessionFactory;
-    private String url = "jdbc:postgresql://localhost:5432/test";
-    private String user = "postgres";
-    private String password = "asdqwe123";
 
     private PeopleRepository() {
         sessionFactory = new  Configuration().configure().buildSessionFactory();
